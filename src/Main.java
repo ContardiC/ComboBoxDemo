2b33900c44803cf70e460b1e36073d1c76938b55
ContardiC <c.contardi@spacecoding.it>
2025-03-12 09:38:39 +0100

    add: new colors in the menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        JFrame frmMain = new JFrame("ComboBoxDemo");
+        frmMain.setLayout(new FlowLayout());
+        frmMain.setSize(640,480);
+        JLabel lblHint = new JLabel("Scegli un colore: ");
+        JLabel lblSelectedColor = new JLabel("... ");
+        JComboBox<String> cmbColors;
+        String[] colors = {"Rosso", "Blu", "Viola", "Rosa", "Giallo"};
+        cmbColors = new JComboBox<>(colors);
+        frmMain.add(lblHint);
+        frmMain.add(cmbColors);
+        frmMain.add(lblSelectedColor);
+
+
+
+
+
+        cmbColors.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedColor = (String) cmbColors.getSelectedItem();
+                lblSelectedColor.setText(selectedColor);
+
+            }
+        });
+        frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmMain.setVisible(true);
     }
 }
